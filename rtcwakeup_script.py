3ccdadbafe19ce59acf5b1ff3909cb392a43f7e5
Jack <jhughes1153@aol.com>
2018-12-19 21:11:43 -0500

    adding rtcwakeup_scipt?

---
+++ rtcwakeup_script.py
@@ -0,0 +1,32 @@
+'''
+Created on Dec 15, 2018
+
+Premise of this module is to run the rtcwakeup command and call this module from cron instead of figureing out the date portion in bash
+
+@author: jack
+'''
+import os
+import datetime as dt
+import time
+import sys
+
+def main():
+    now = dt.datetime.now()
+    
+    with open('/home/jack/timecheck.txt', 'w') as cron_checker:
+        cron_checker.write('Good evening the time is {} the crontab worked!!!'.format(now))
+    
+    now = now + dt.timedelta(days=7)
+    now = now.replace(minute=0, second=0, microsecond=0)
+    
+    a = """sudo rtcwake -m off -l -t "$(date -d '{}' '+%s')" """.format(now)
+    
+    print(a)
+    os.system("""sudo rtcwake -m no -l -t "$(date -d '{}' '+%s')" """.format(now))
+    
+    time.sleep(5)
+    
+    os.system("""sudo poweroff""")
+
+if __name__ == '__main__':
+    sys.exit(main())
